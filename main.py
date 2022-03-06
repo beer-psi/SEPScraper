b972269d16fc32d1c227b9e1226155763817742b
beerpiss <lacvtg.a1.2023@gmail.com>
2022-03-06 12:14:00 +0700

    clean up code

--- main.py
+++ main.py
@@ -5,59 +5,38 @@ import asyncio
 import pathlib
 import aiopath
 import aiofiles
-import posixpath
 import logging
 from requests_futures.sessions import FuturesSession
 from typing import Union
 from utils import *
 
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(name)s: %(message)s')
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s %(levelname)s %(name)s: %(message)s"
+)
 
 
-async def save_signed_sepbb(device):
+async def save_all_signed_sepbb(device):
     logging.info(f"Getting signing status for {device['identifier']}")
-    signed_ipsw = await get_signing_status(device['identifier'])
+    signed_ipsw = await get_signing_status(device["identifier"])
 
     for fw in signed_ipsw:
         logging.info(f"Getting build manifest for firmware {fw['buildid']}")
         async with aiofiles.tempfile.TemporaryDirectory() as tmpdir:
-            manifest = await get_manifest(fw['url'], posixpath.join(tmpdir, f"{fw['buildid']}.plist"))
-        
-            with open(manifest, 'rb') as f:
+            manifest = await get_manifest(
+                fw["url"], aiopath.AsyncPath(tmpdir) / f"{fw['buildid']}.plist"
+            )
+
+            with open(manifest, "rb") as f:
                 buildmanifest = plistlib.loads(f.read())
-        boards = [x for x in device['boards'] if x['boardconfig'].lower().endswith('ap')]
-        for board in boards:
-            try:
-                buildidentity = next(
-                    x for x in buildmanifest['BuildIdentities']
-                    if x['Info']['DeviceClass'].lower() == board['boardconfig'].lower()
-                )
-            except StopIteration:
-                logging.error("Couldn't get data from BuildManifest")
-                continue
-
-            if 'RestoreSEP' in buildidentity['Manifest']:
-                sep_path = buildidentity['Manifest']['RestoreSEP']['Info']['Path']
-                logging.info(f"[{device['identifier']}, {board['boardconfig']}, {fw['buildid']}] Found SEP firmware at {sep_path}")
-            else:
-                sep_path = None
-                logging.warning(f"[{device['identifier']}, {board['boardconfig']}, {fw['buildid']}] Couldn't find SEP firmware")
-
-            if 'BasebandFirmware' in buildidentity['Manifest']:
-                bb_path = buildidentity['Manifest']['BasebandFirmware']['Info']['Path']
-                logging.info(f"[{device['identifier']}, {board['boardconfig']}, {fw['buildid']}] Found baseband firmware at {bb_path}")
-            else:
-                bb_path = None
-                logging.warning(f"[{device['identifier']}, {board['boardconfig']}, {fw['buildid']}] Couldn't find baseband firmware")
-
-            logging.info(f"Downloading")
-            output_path = aiopath.AsyncPath(f"./output/{fw['version'].replace(' ', '-')}_{fw['buildid']}/{device['identifier']}/{board['boardconfig']}")
-            await output_path.mkdir(parents=True, exist_ok=True)
-            if sep_path:
-                pzb(fw['url'], sep_path, output_path / posixpath.split(sep_path)[-1])
-            if bb_path:
-                pzb(fw['url'], bb_path, output_path / posixpath.split(bb_path)[-1])
+        boards = [
+            x for x in device["boards"] if x["boardconfig"].lower().endswith("ap")
+        ]
+        tasks = [
+            asyncio.create_task(save_sepbb(device, board, fw, buildmanifest))
+            for board in boards
+        ]
+        await gather_with_concurrency(2, *tasks)
 
 
 async def main():
@@ -66,7 +45,7 @@ async def main():
         devices = session.get("https://api.ipsw.me/v4/devices").result().json()
 
     for device in devices:
-        await save_signed_sepbb(device)
+        await save_all_signed_sepbb(device)
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import plistlib
 import remotezip
 import aiopath
 import asyncio
+import logging
 from requests_futures.sessions import FuturesSession
 from typing import Union
 
@@ -18,54 +19,166 @@ async def gather_with_concurrency(n, *tasks):
     async def sem_task(task):
         async with semaphore:
             return await task
+
     return await asyncio.gather(*(sem_task(task) for task in tasks))
 
 
 def pzb(url: str, filename: str, output_path: str) -> Union[bool, aiopath.AsyncPath]:
+    """Extracts a file from a remote ZIP file.
+
+    Args:
+        url (str): URL of zip file
+        filename (str): File to extract from zip
+        output_path (str): Where to save the extracted file
+
+    Returns:
+        Union[bool, aiopath.AsyncPath]: Returns the path if extraction was successful, otherwise returns False.
+    """
     try:
         with remotezip.RemoteZip(url) as ipsw:
             file = ipsw.read(filename)
     except remotezip.RemoteIOError:
         return False
 
-    with open(output_path, 'wb') as f:
+    with open(output_path, "wb") as f:
         f.write(file)
 
     return aiopath.AsyncPath(output_path)
 
 
 async def get_manifest(url: str, path: str) -> Union[bool, aiopath.AsyncPath]:
+    """Gets BuildManifest.plist from an IPSw url
+
+    Args:
+        url (str): Download link for an IPSW
+        path (str): Path to save the BuildManifest to (including filename)
+
+    Returns:
+        Union[bool, aiopath.AsyncPath]: Returns the path if saving was successful, otherwise returns False.
+    """
     with FuturesSession() as session:
-        resp = session.get(f"{'/'.join(url.split('/')[:-1])}/BuildManifest.plist").result()
+        resp = session.get(
+            f"{'/'.join(url.split('/')[:-1])}/BuildManifest.plist"
+        ).result()
     if resp.status_code == 200:
         manifest = resp.content
 
-        with open(path, 'wb') as f:
+        with open(path, "wb") as f:
             f.write(manifest)
 
         return aiopath.AsyncPath(path)
     else:
-        return await asyncio.to_thread(pzb, url, "BuildManifest.plist", path) or await asyncio.to_thread(pzb, url, "BuildManifesto.plist", path)  
+        return await asyncio.to_thread(
+            pzb, url, "BuildManifest.plist", path
+        ) or await asyncio.to_thread(pzb, url, "BuildManifesto.plist", path)
 
 
-async def get_signing_status(identifier):
+async def get_signing_status(identifier: str) -> list:
+    """Get all signed versions for a device.
+
+    Args:
+        identifier (str): Device identifier (e.g. iPhone10,5)
+
+    Returns:
+        list: List of all currently signed versions for the device
+    """
     with FuturesSession() as session:
         beta_status_future = session.get(f"https://api.m1sta.xyz/betas/{identifier}")
-        status_future = session.get(f"https://api.ipsw.me/v4/device/{identifier}?type=ipsw")
+        status_future = session.get(
+            f"https://api.ipsw.me/v4/device/{identifier}?type=ipsw"
+        )
         beta_status = beta_status_future.result()
         if beta_status.status_code == 200:
             beta_status = beta_status.json()
-            beta_status_list = [x for x in beta_status if 'signed' in x and x['signed']]
+            beta_status_list = [x for x in beta_status if "signed" in x and x["signed"]]
         else:
             beta_status_list = []
         status = status_future.result()
         if status.status_code == 200:
             status = status.json()
-            status_list = [x for x in status['firmwares'] if 'signed' in x and x['signed']]
+            status_list = [
+                x for x in status["firmwares"] if "signed" in x and x["signed"]
+            ]
         else:
             status_list = []
     return status_list + beta_status_list
 
 
+async def save_sepbb(
+    device: dict, board: dict, firm: dict, manifest: dict
+) -> Union[aiopath.AsyncPath, bool]:
+    """Saves SEP and/or baseband firmware for one device, on one board, on one version.
 
+    Args:
+        device (dict): The device to save SEP/Baseband for. It should have the `identifier` key.
+        board (dict): The boardconfig of the device.
+        firm (dict): The firmware to save SEP/Baseband for.
+        manifest (dict): The build manifest for that firmware. Must be loaded through plistlib.
 
+    Returns:
+        Union[aiopath.AsyncPath, bool]: Returns the folder containing SEP/Baseband firmware on success, False on failure.
+
+    Notes:
+        Check https://api.ipsw.me/v4/devices and https://api.ipsw.me/v4/device/:identifier to see how device, board and firm looks like.
+    """
+    save_path = aiopath.AsyncPath(
+        f"./output/{firm['version'].replace(' ', '-')}_{firm['buildid']}/{device['identifier']}/{board['boardconfig']}"
+    )
+
+    try:
+        buildidentity = next(
+            x
+            for x in manifest["BuildIdentities"]
+            if x["Info"]["DeviceClass"].lower() == board["boardconfig"].lower()
+        )
+    except StopIteration:
+        logging.error("Couldn't get data from BuildManifest")
+        return False
+
+    if "RestoreSEP" in buildidentity["Manifest"]:
+        sep_path = buildidentity["Manifest"]["RestoreSEP"]["Info"]["Path"]
+        logging.info(
+            f"[{device['identifier']}, {board['boardconfig']}, {firm['buildid']}] Found SEP firmware at {sep_path}"
+        )
+    else:
+        sep_path = None
+        logging.warning(
+            f"[{device['identifier']}, {board['boardconfig']}, {firm['buildid']}] Couldn't find SEP firmware"
+        )
+
+    if "BasebandFirmware" in buildidentity["Manifest"]:
+        bb_path = buildidentity["Manifest"]["BasebandFirmware"]["Info"]["Path"]
+        logging.info(
+            f"[{device['identifier']}, {board['boardconfig']}, {firm['buildid']}] Found baseband firmware at {bb_path}"
+        )
+    else:
+        bb_path = None
+        logging.warning(
+            f"[{device['identifier']}, {board['boardconfig']}, {firm['buildid']}] Couldn't find baseband firmware"
+        )
+
+    if sep_path or bb_path:
+        logging.info(
+            f"[{device['identifier']}, {board['boardconfig']}, {firm['buildid']}] Saving SEP and/or BB firmware to {save_path}"
+        )
+        await save_path.mkdir(parents=True, exist_ok=True)
+        if sep_path:
+            await asyncio.to_thread(
+                pzb,
+                firm["url"],
+                sep_path,
+                save_path / aiopath.AsyncPath(sep_path).parts[-1],
+            )
+        if bb_path:
+            await asyncio.to_thread(
+                pzb,
+                firm["url"],
+                bb_path,
+                save_path / aiopath.AsyncPath(bb_path).parts[-1],
+            )
+        return save_path
+    else:
+        logging.warning(
+            f"[{device['identifier']}, {board['boardconfig']}, {firm['buildid']}] There is no SEP/BB for this IPSW"
+        )
+        return False
